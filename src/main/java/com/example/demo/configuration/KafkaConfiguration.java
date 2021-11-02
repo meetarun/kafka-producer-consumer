e7d50f6672eb014b5b66b211e6aed39585943251
meetarun <meetarun@live.com>
2021-11-02 15:40:49 +0530

    kafka config update

--- src/main/java/com/example/demo/configuration/KafkaConfiguration.java
+++ src/main/java/com/example/demo/configuration/KafkaConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -18,6 +19,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@EnableKafka
 @Configuration
 public class KafkaConfiguration {
 
